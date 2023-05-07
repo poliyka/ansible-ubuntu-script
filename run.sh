5220f995d008a353ec8a45bbcba1ab64928bd4f9
Ubuntu <poliyka@node1.tdcz4inpikcezc3osucwoqea3a.hx.internal.cloudapp.net>
2023-05-07 03:47:21 +0000

    fix bug

--- run.sh
+++ run.sh
@@ -3,18 +3,35 @@
 ENV_FILE="ansible.env"
 
 # 讀取 .env file
-set -o allexport # enable all variable definitions to be exported
-source <(sed -e "s/\r//" -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/=\"\1\"/g" "${ENV_FILE}")
-set +o allexport
+source $ENV_FILE
+export PATH="$HOME/.local/bin:$PATH"
+
+while read -r line; do
+  if [ -n "$line" ]; then
+    echo "${line%%=*}: ${line#*=}"
+  fi
+done < ansible.env > ./vars/env.yaml
+
+
+
+# python3.8
+sudo apt install python3.8 python3-setuptools -y
+echo "1" | sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+sudo update-alternatives --config python
+curl https://bootstrap.pypa.io/get-pip.py | python
+
 
 # 確定 Ansible 指令是否存在
 if ! command -v ansible-playbook
 then
-python3 -m pip install ansible
+python -m pip install ansible
 fi
+
+
 
 # 安裝第三方模組
 ansible-galaxy collection install community.general
 
 # 執行 Ansible
 ansible-playbook -i inventory playbook.yml --extra-vars "ansible_become_pass=${OE_PASSWORD}"
+
